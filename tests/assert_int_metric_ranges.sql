4b40e3b7664aafffafd297891101bfa780f89fd5
TheMaxfly <maxime.mcfly@gmail.com>
2026-04-01 16:33:12 +0200

    fix: align dbt metric test with french tip field

--- tests/assert_int_metric_ranges.sql
+++ tests/assert_int_metric_ranges.sql
@@ -2,4 +2,4 @@ select *
 from {{ ref('int_trip_metrics') }}
 where trip_duration_minutes not between 1 and 300
    or avg_speed_mph < 0
-   or (tip_percentage is not null and tip_percentage < 0)
+   or (taux_pourboire is not null and taux_pourboire < 0)
